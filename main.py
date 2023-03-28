ea9cddde4816c5a60db50686591afd5c8ec05582
jomid314 <66674156+jomid314@users.noreply.github.com>
2023-03-28 20:37:57 +0800

    add content from each text file to the PDF

--- main.py
+++ main.py
@@ -15,4 +15,9 @@ for filepath in filepaths:
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=name, ln=1)
 
+    with open(filepath, "r") as file:
+        content = file.read()
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, txt=content)
+
 pdf.output("output.pdf")
